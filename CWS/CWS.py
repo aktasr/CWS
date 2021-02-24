df7a89c9e16711a7e62aed1581f7d70af2feb927
LAPTOP-5ILEASDF\Ramazan <raktas@netas.com.tr>
2021-02-24 22:58:21 +0300

    Binance Added

---
+++ CWS/CWS.py
@@ -0,0 +1,213 @@
+#Author      : Ramazan AKTAS
+#Date        : 14.02.2021 23:00
+#Description : Usage examples
+
+from btcturk import btcturk
+from binance import binance
+import time
+import asyncio
+from concurrent.futures import ProcessPoolExecutor
+import threading
+
+def onOrderbook(*args):
+    print(args[0])
+
+def onMessage(*args):
+    print(args[0])
+
+def start():
+    ''' start func '''
+    
+    ''' user can be subscribe one or more market(symbol). F.e: suscriber_trade = ['BTCTRY', 'BTCUSDT']
+    also user can be subscribe all markets(symbols). F.e: suscriber_trade = ['all'] '''
+
+    btc = btcturk(subscribe_orderbook=['BTCTRY','BTCUSDT'])
+    bnc = binance(subscribe_orderbook=['BTCUSDT', 'BTCTRY'])
+    
+    ''' creates websocket, don't subscribe anything. After connection established, you can subscribe any channel for any symbol 
+    command out below code and command above code to try it.'''
+    #btc = btcturk()
+
+    ''' to prevent exception on manual subscription, you can check isConnected flag for a time: 
+    command out below code to try it.'''
+    #MaxTryCount = 10 
+    #tryCount = 0
+    #try:
+    #    while tryCount < MaxTryCount:          
+    #        if btc.isConnected is False:
+    #            time.sleep(0.5)
+    #            continue
+    #        else: 
+    #            break
+
+    #    btc.subscribe_orderbook('BTCTRY')
+    #except :
+    #    print("\nTried to subscribe before connection!!!\n")
+
+
+
+    # start to thread for continuous websocket communication
+    thr1 = threading.Thread(target=btc.start)
+    thr1.daemon = True
+    thr1.start()
+    
+    thr = threading.Thread(target=bnc.start)
+    thr.daemon = True
+    thr.start()
+
+    ''' parsed orderbook message is can be directly used in this func. 
+    It can be used if want to use synchronously 
+                      Symbol       Bids    Price   Amount     Asks     Price  Amount
+    Format example: {'BTCUSDT' : {'bids':[[40000, 0.980][]], 'asks': [[40100, 0.980][]]}}
+    When websocket sends an orderbook message, this event is called in websocket thread.'''    
+    
+    #btc.onOrderbookMsg = onOrderbook
+
+    ''' raw data is can be directly used in this func.
+    It can be used if want to use synchronously.
+    Command out below code to try. '''
+    #btc.onMsg = onMessage
+
+    '''Also parsed orderbook message is can be achieved at any time from another thread by using fetch_orderbook() function.
+    It can be custumized giving symbol as a parameter f.e: fetch_orderbook(symbol = 'BTCTRY')
+    It can be used if want to use asynchronously 
+    Command out below code to try it'''
+    #while True:
+    #  print(btc.fetch_orderbook(symbol='BTCTRY'))
+    #  time.sleep(1)
+
+    testDelay = 0.1
+    testDuration = 600/testDelay
+    duration = 0
+    while duration < testDuration:
+        testBtcTurkChangeCount(btc,'BTCUSDT')
+        testBinanceChangeCount(bnc,'BTCUSDT')
+        duration += 1
+        #print("BtcTurk BTCTRY: ",btc.fetch_orderbook(symbol='BTCTRY'))
+        #print("BtcTurk BTCUSDT: ",btc.fetch_orderbook(symbol='BTCUSDT'))
+        #print("Binance BTCTRY: ",bnc.fetch_orderbook(symbol='BTCTRY'))
+        #print("Binance BTCUSDT: ",bnc.fetch_orderbook(symbol='BTCUSDT'))
+        time.sleep(testDelay)
+        
+    
+
+    print("\nTest finished. Test duration is ", testDuration," ms with test delay", testDelay, " ms.")
+    print("\nBtcTurkAskChangeCount: ",BtcTurkAskChangeCount, "\nBtcTurkBidChangeCount: ", BtcTurkBidChangeCount)
+    print("\nBinanceAskChangeCount: ",BinanceAskChangeCount, "\nBinanceBidChangeCount: ", BinanceBidChangeCount)
+    #task = asyncio.create_task(deneme())
+
+    
+
+    #while True:
+    #    time.sleep(20)
+
+
+    #asyncio.run(test2())
+
+
+    #time.sleep(1)
+    #btc.subscribe_ticker('BTCTRY')
+    #time.sleep(1)
+    #btc.subscribe_trade('BTCTRY')
+
+BtcTurkAskChangeCount = 0
+BtcTurkBidChangeCount = 0
+BtcTurkLastAsk = None
+BtcTurkLastBid = None
+
+BinanceAskChangeCount = 0
+BinanceBidChangeCount = 0
+BinanceLastAsk = None
+BinanceLastBid = None
+
+def testBtcTurkChangeCount(btc, symbol):
+    global BtcTurkLastAsk
+    global BtcTurkLastBid
+    global BtcTurkAskChangeCount
+    global BtcTurkBidChangeCount
+
+    data = btc.fetch_orderbook(symbol=symbol)
+    if data == None:
+        return
+
+    ask = data['asks'][0][0]
+    bid = data['bids'][0][0]
+
+    if BtcTurkLastBid == None:
+        BtcTurkLastBid = bid
+        return
+    elif BtcTurkLastBid == bid:
+        return
+    else:
+        BtcTurkBidChangeCount += 1
+        BtcTurkLastBid = bid
+
+    if BtcTurkLastAsk == None:
+        BtcTurkLastAsk = ask
+        return
+    elif BtcTurkLastAsk == ask:
+        return
+    else:
+        BtcTurkAskChangeCount += 1
+        BtcTurkLastAsk = ask
+
+
+def testBinanceChangeCount(btc, symbol):
+    global BinanceAskChangeCount
+    global BinanceBidChangeCount
+    global BinanceLastAsk
+    global BinanceLastBid
+
+    data = btc.fetch_orderbook(symbol=symbol)
+    if data == None:
+        return
+
+    ask = data['asks'][0][0]
+    bid = data['bids'][0][0]
+
+    if BinanceLastBid == None:
+        BinanceLastBid = bid
+        return
+    elif BinanceLastBid == bid:
+        return
+    else:
+        BinanceBidChangeCount += 1
+        BinanceLastBid = bid
+
+    if BinanceLastAsk == None:
+        BinanceLastAsk = ask
+        return
+    elif BinanceLastAsk == ask:
+        return
+    else:
+        BinanceAskChangeCount += 1
+        BinanceLastAsk = ask
+
+def test():
+    while True:
+        print("Deniyoruz")
+        print("thread count in test:", threading.active_count())
+        time.sleep(1)
+
+async def test2():
+    task = asyncio.create_task(test3())
+    while True:
+        print("test2")
+        print("thread count in test2:", threading.active_count())
+        await asyncio.sleep(1)
+
+
+async def test3():
+    count = 5
+    while count>0:
+        count-=1
+        print("test3")
+        await asyncio.sleep(1)
+
+if __name__ == "__main__":
+    #asyncio.run(start())
+    start()
+
+
+
+
---
+++ CWS/binance.py
@@ -0,0 +1,107 @@
+#Author : Ramazan AKTAS
+#Date   : 20.02.2021 22:30
+
+
+from base.exchange import exchange
+import time
+
+class binance(exchange):
+    """description of class"""
+
+    # user can be subscribe one or more market(symbol). F.e: suscriber_trade = ['BTCTRY', 'BTCUSDT']
+    # also user can be subscribe all markets(symbols). F.e: suscriber_trade = ['all']
+    def __init__(self, subscribe_ticker:list = [], subscribe_trade:list = [], subscribe_orderbook:list = [], subscribe_obDiff:list = []):
+        super().__init__({ 'onOpenEvent' : {
+            'subscribe_ticker' : subscribe_ticker,
+            'subscribe_trade' : subscribe_trade,
+            'subscribe_orderbook' : subscribe_orderbook,
+            'subscribe_orderbookDiff' : subscribe_obDiff
+            }})
+        self.connect()
+
+    def describe(self):
+        return self.deep_extend(super().describe(), {
+            'id':'binance', 
+            'name':'BINANCE', 
+            'url':'wss://stream.binance.com:9443/ws',
+            'wsLogEnable' : 'False',
+            'Subscribe' : 'SUBSCRIBE', 
+            'Result' :  'result',
+            'OrderBook' : 'depth20',
+            'OrderBookDifference' : 'depth',
+            'TickerAll' : 401,
+            'TickerPair' : 402,
+            'Channel':['ticker','trades','depth20@100ms','depth'],
+            'Symbols': {
+                'BTCUSDT' : 'btcusdt',
+                'BTCTRY' : 'btctry',
+                'ALL' : 'all',
+                },
+            'has': {
+                'tickerSubscribe': True,
+                'tradeSubscribe': True,
+                'orderbookSubscribe': True,
+                'orderbookDiffSubscribe': True
+                }})
+
+    def subscribe_ticker(self, symbol, params={}):
+        try: 
+            self.subscribe(self.Channel[0], self.Symbols[symbol])
+        except:
+            print("Holaa")
+     
+    def subscribe_trade(self, symbol, params={}):
+        self.subscribe(self.Channel[1],self.Symbols[symbol])
+    
+    def subscribe_orderbook(self, symbolArr:list = [], params = {}):        
+        symbols = []
+        for x in symbolArr:
+            symbols.append(self.Symbols[x])
+
+        self.subscribe(self.Channel[2],symbols)
+            
+    def subscribe_orderbookDiff(self, symbol, params={}):
+        self.subscribe(self.Channel[3],self.Symbols[symbol])
+
+    def subscribe(self, *params):
+        if self.isConnected is False:
+            raise PermissionError('Not connected to websocket server.')
+
+        subscribeParams = []
+        symbols = params[1]
+        channel = params[0]
+
+        for symbol in symbols:
+            param = symbol + "@" + channel
+            subscribeParams.append(param)
+
+        jsonData = {"method": self.Subscribe, "params" : subscribeParams, "id" : 1}
+        self.sendMessage(self.json(jsonData))
+
+    def orderbook_received(self, message):
+        symbol = self.getSymbolFromStream(message['stream'])
+        orderbook = message['data']
+
+        self.parse_orderbook(symbol, orderbook, 0)
+    
+    def onMessage(self, message):
+        channel = self.getChannelFromStream(message['stream'])
+        self.parseAndRouteMessage(channel, message)
+
+    def onOpen(self):
+        ''' in this project, combined subscribe is used. Response data must include owner stream information. '''
+        data = {"method": "SET_PROPERTY", "params": ["combined",True],"id": 5}
+        self.sendMessage(self.json(data))
+        super().onOpen()
+
+    def getSymbolFromStream(self, stream):
+        ''' class specific function '''
+        return (self.parseStream(stream)[0])
+    
+    def getChannelFromStream(self, stream):
+        ''' class specific function '''
+        return (self.parseStream(stream)[1])
+    
+    def parseStream(self, stream):
+        ''' class specific function '''
+        return stream.split("@")
